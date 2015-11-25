55ee382c9cf7ac1c4b5e4a8090b82c328b4c4d96
attakei <attakei@gmail.com>
2015-11-25 10:55:25 +0900

    Use scan by venusian

---
+++ tests/test_scanner.py
@@ -0,0 +1,17 @@
+# -*- coding:utf8 -*-
+"""Test for Ananta scanning
+"""
+from ananta import FunctionCollector
+
+
+lambda_collection = FunctionCollector()
+lambda_config = lambda_collection.lambda_config
+
+
+def test_scan():
+    import venusian
+    from . import tests_scan
+    scanner = venusian.Scanner()
+    scanner.scan(tests_scan)
+    assert len(lambda_collection.functions) == 2
+    assert lambda_collection.functions[0]['name'] in ('my_func_1', 'my_func_2')
