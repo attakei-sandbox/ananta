ddbddc12b20bfb24409c719e8402b8252e63ff43
attakei <attakei@gmail.com>
2016-01-08 13:11:37 +0900

    [WIP] Scan target is enabled to specify by config.

--- ananta/scripts/build.py
+++ ananta/scripts/build.py
@@ -19,11 +19,11 @@ def build_packages(registry, config, args):
         args.path = tempfile.mkdtemp()
     pip_args = 'install -t {} .'.format(args.path)
     pip.main(pip_args.split(' '))
-    scan_all(registry, args.path)
+    scan_all(registry, args.path, config.get('ananta:build', 'Target'))
     shutil.make_archive('package', 'zip', root_dir=args.path)
 
 
-def scan_all(registry, package_dir):
+def scan_all(registry, package_dir, target=None):
     import venusian
     modules = []
     sys.path.append(package_dir)
@@ -35,6 +35,8 @@ def scan_all(registry, package_dir):
         else:
             continue
         modules.append(importlib.import_module(module_name))
+    target_modules = [t.strip() for t in target.split('\n') if t != '']
+    modules = [module for module in modules if module.__name__ in target_modules]
     scanner = venusian.Scanner(registry=registry)
     for module in modules:
         scanner.scan(module)
