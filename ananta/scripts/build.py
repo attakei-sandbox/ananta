96df9e15c0eff98afa3ac28af68dce41dc9146a4
attakei <attakei@gmail.com>
2016-01-08 13:17:32 +0900

    Fix to be runnable no config

--- ananta/scripts/build.py
+++ ananta/scripts/build.py
@@ -19,7 +19,11 @@ def build_packages(registry, config, args):
         args.path = tempfile.mkdtemp()
     pip_args = 'install -t {} .'.format(args.path)
     pip.main(pip_args.split(' '))
-    scan_all(registry, args.path, config.get('ananta:build', 'Target'))
+    if config:
+        target = config.get('ananta:build', 'Target', [])
+    else:
+        target = None
+    scan_all(registry, args.path, target)
     shutil.make_archive('package', 'zip', root_dir=args.path)
 
 
@@ -35,8 +39,9 @@ def scan_all(registry, package_dir, target=None):
         else:
             continue
         modules.append(importlib.import_module(module_name))
-    target_modules = [t.strip() for t in target.split('\n') if t != '']
-    modules = [module for module in modules if module.__name__ in target_modules]
+    if target:
+        target_modules = [t.strip() for t in target.split('\n') if t != '']
+        modules = [module for module in modules if module.__name__ in target_modules]
     scanner = venusian.Scanner(registry=registry)
     for module in modules:
         scanner.scan(module)
